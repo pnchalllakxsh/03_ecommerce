166dcc73d5c8ef59de1eb33aa595428859f5013a
lakshya Panchal <panchallakshya64@gmail.com>
2024-10-19 16:19:16 +0530

    new version

--- script.js
+++ script.js
@@ -1,68 +1,83 @@
-document.addEventListener("DOMContentLoaded", () => {
+
+document.addEventListener("DOMContentLoaded",()=>{
+
   const products = [
-    { id: 1, name: "Product 1", price: 29.99 },
-    { id: 2, name: "Product 2", price: 19.99 },
-    { id: 3, name: "Product 3", price: 59.999 },
-  ];
+        { id: 1, name: "Product 1", price: 29.99 },
+        { id: 2, name: "Product 2", price: 19.99 },
+        { id: 3, name: "Product 3", price: 59.999 },
+      ];
 
-  const cart = [];
-
-  const productList = document.getElementById("product-list");
-  const cartItems = document.getElementById("cart-items");
-  const emptyCartMessage = document.getElementById("empty-cart");
-  const cartTotalMessage = document.getElementById("cart-total");
-  const totalPriceDisplay = document.getElementById("total-price");
-  const checkOutBtn = document.getElementById("checkout-btn");
-
-  products.forEach((product) => {
-    const productDiv = document.createElement("div");
-    productDiv.classList.add("product");
-    productDiv.innerHTML = `
-    <span>${product.name} - $${product.price.toFixed(2)}</span>
-    <button data-id="${product.id}">Add to cart</button>
-    `;
+  let cart = [];
+  const productList = document.querySelector("#product-list")
+  const cartItems = document.querySelector("#cart-items")
+  const emptyCartMessage =  document.querySelector("#empty-cart")
+  const cartTotalDisplay = document.querySelector("#cart-total")
+  const totalPriceDisplay = document.querySelector("#total-price")
+  const checkoutButton = document.querySelector("#checkout-btn")
+  
+  products.forEach(product => {
+    let productDiv = document.createElement('div');
+    productDiv.classList.add('product');
+    productDiv.innerHTML = `<span>${product.name} - $${product.price}</span>
+    <button buttonId ="${product.id}" >Add</button>`
     productList.appendChild(productDiv);
-  });
+  })
 
-  productList.addEventListener("click", (e) => {
-    if (e.target.tagName === "BUTTON") {
-      const productId = parseInt(e.target.getAttribute("data-id"));
-      const product = products.find((p) => p.id === productId);
-      addToCart(product);
+  productList.addEventListener('click',(e)=>
+  {
+    if(e.target.tagName=="BUTTON")
+    {
+      e.stopPropagation();
+      const productId = parseInt(e.target.getAttribute("buttonId"));
+
+      // const item = products.find(product => product.id===productId);
+      // another way
+      const [item] = products.filter(product => 
+        product.id === productId);
+      addToCart(item);
+
     }
-  });
 
-  function addToCart(product) {
+  })
+
+  function addToCart(product){
     cart.push(product);
     renderCart();
   }
 
-  function renderCart() {
-    cartItems.innerText = "";
-    let totalPrice = 0;
-
-    if (cart.length > 0) {
+  function renderCart(){
+    cartItems.innerHTML = ""; // this is important
+    if(cart.length>0)
+    {
       emptyCartMessage.classList.add("hidden");
-      cartTotalMessage.classList.remove("hidden");
-      cart.forEach((item, index) => {
-        totalPrice += item.price;
-        const cartItem = document.createElement("div");
-        cartItem.innerHTML = `
-        ${item.name} - $${item.price.toFixed(2)}
-        `;
-        cartItems.appendChild(cartItem);
-        totalPriceDisplay.textContent = `${totalPrice.toFixed(2)}`;
-      });
-    } else {
+      cartTotalDisplay.classList.remove("hidden");
+
+      let total=0;
+      cart.forEach((product)=>{
+        total += product.price;
+
+        let item = document.createElement('div');
+        item.classList.add('product');
+        item.innerHTML = `<span>${product.name} - $${product.price}</span>`
+        cartItems.appendChild(item);
+
+        totalPriceDisplay.textContent = total;
+      })
+    }
+    else
+    {
       emptyCartMessage.classList.remove("hidden");
-      cartTotalMessage.classList.add("hidden");
-      totalPriceDisplay.textContent = `$0.00`;
+      cartTotalDisplay.classList.add("hidden");
     }
   }
 
-  checkOutBtn.addEventListener("click", () => {
-    cart.length = 0;
-    alert("Checkout successfully");
+  checkoutButton.addEventListener("click", () => {
+    cart.length=0;
+    emptyCartMessage.classList.remove("hidden");
+    cartTotalDisplay.classList.add("hidden");
+    // cartItems.innerHTML = "";
     renderCart();
+
   });
+
 });
